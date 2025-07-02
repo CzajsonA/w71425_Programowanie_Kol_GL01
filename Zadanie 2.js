fb29e26a3725462187e2ebabf885f587c2cafba6
CzajsonA <aczaja97@gmail.com>
2025-07-02 11:06:07 +0200

    Zadanie 2.js

--- Zadanie 2.js
+++ Zadanie 2.js
@@ -1,28 +1,53 @@
 class Ksiazka {
     constructor(tytul, rokWydania, imieAutora, nazwiskoAutora) {
-        this.tytul = tytul;
-        this.rokWydania = rokWydania;
-        this.imieAutora = imieAutora;
-        this.nazwiskoAutora = nazwiskoAutora;
+      this.tytul = tytul;
+      this.rokWydania = rokWydania;
+      this.imieAutora = imieAutora;
+      this.nazwiskoAutora = nazwiskoAutora;
+      this.iloscWypozyczen = 0; 
     }
-
+  
     pelneImieINazwiskoAutora() {
-        return `${this.imieAutora} ${this.nazwiskoAutora}`;
+      return `${this.imieAutora} ${this.nazwiskoAutora}`;
     }
-}
-
-const listaKsiazek = [
+  
+    ustawIloscWypozyczen(ilosc) {
+      this.iloscWypozyczen = ilosc;
+    }
+  
+    zwiekszIloscWypozyczen() {
+      this.iloscWypozyczen += 1;
+    }
+  }
+  
+  const ksiazki = [
     new Ksiazka("Lalka", 1890, "Bolesław", "Prus"),
-    new Ksiazka("Quo Vadis", 1896, "Henryk", "Sienkiewicz"),
     new Ksiazka("Pan Tadeusz", 1834, "Adam", "Mickiewicz"),
-    new Ksiazka("Ferdydurke", 1937, "Witold", "Gombrowicz")
-];
-
-const wybranyRok = 1896;
-
-console.log(`Książki wydane w roku ${wybranyRok}:`);
-listaKsiazek.forEach(ksiazka => {
+    new Ksiazka("Ferdydurke", 1937, "Witold", "Gombrowicz"),
+    new Ksiazka("Quo Vadis", 1896, "Henryk", "Sienkiewicz"),
+  ];
+  
+  const wybranyRok = 1896;
+  
+  console.log(`Książki wydane w roku ${wybranyRok}:`);
+  ksiazki.forEach(ksiazka => {
     if (ksiazka.rokWydania === wybranyRok) {
-        console.log(`"${ksiazka.tytul}" - autor: ${ksiazka.pelneImieINazwiskoAutora()}`);
+      console.log(`Tytuł: "${ksiazka.tytul}", Autor: ${ksiazka.pelneImieINazwiskoAutora()}`);
     }
-});
+  });
+  
+  ksiazki[0].ustawIloscWypozyczen(71425);
+  
+  let najstarszaKsiazka = ksiazki[0];
+  ksiazki.forEach(ksiazka => {
+    if (ksiazka.rokWydania < najstarszaKsiazka.rokWydania) {
+      najstarszaKsiazka = ksiazka;
+    }
+  });
+  
+  console.log(`\nNajstarsza książka: "${najstarszaKsiazka.tytul}" (rok: ${najstarszaKsiazka.rokWydania})`);
+  
+  const sumaWypozyczen = ksiazki.reduce((suma, ksiazka) => suma + ksiazka.iloscWypozyczen, 0);
+  
+  console.log(`\nŁączna liczba wypożyczeń wszystkich książek: ${sumaWypozyczen}`);
+  
